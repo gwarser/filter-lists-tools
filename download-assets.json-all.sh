17fc59bd9a422830462d218c4040d46d0c46418d
gwarser <gwarser@gmail.com>
2020-02-28 23:27:26 +0100

    Fix logging curl return value in assets.json failure log

--- download-assets.json-all.sh
+++ download-assets.json-all.sh
@@ -56,7 +56,7 @@ do
     else
         ret=$?
         echo -e "$H# Downloading failed$L"
-        echo -e "$?:\t$id\t$url\t$name" >> "assets.json-failed-downloads.txt"
+        echo -e "$(date +%F):\t($ret):\t$id\t$url\t$name" >> "assets.json-failed-downloads.txt"
     fi
 
 done < assets.json-id-url-name.txt
